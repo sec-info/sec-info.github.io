09d34bf2bf9f2d4e6fd8d01ec7cf92eaad1d5b07
tommysuriel <tommy.suriel@hotmail.com>
2025-04-17 16:46:01 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -305,4 +305,18 @@ darkModeToggle.addEventListener('change', () => {
     localStorage.setItem('darkMode', 'disabled');
   }
 });
+// Scroll to Top Button
+const scrollTopBtn = document.getElementById("scrollTopBtn");
+
+window.onscroll = function () {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    scrollTopBtn.style.display = "block";
+  } else {
+    scrollTopBtn.style.display = "none";
+  }
+};
+
+scrollTopBtn.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
 
